01429b02a69dc38215a44c0efda1f1228501d9f5
Szczepan <szczepan.staszak@gmail.com>
2023-01-05 21:49:41 +0100

    Removed unused code from apator08

--- components/wmbus/apator_08.h
+++ components/wmbus/apator_08.h
@@ -16,32 +16,13 @@ struct Apator08: Driver
 {
   Apator08() : Driver(std::string("apator08")) {};
   bool get_value(std::vector<unsigned char> &telegram, float &water_usage) override {
-    bool ret_val = false;
+    bool ret_val = true;
     uint32_t usage = 0;
-    // std::vector<unsigned char> key(16,0);
-    // std::vector<unsigned char>::iterator pos;
-    // pos = telegram.begin();
-    // unsigned char iv[16];
-    // int i=0;
-    // for (int j=0; j<8; ++j) {
-    //   iv[i++] = telegram[2+j];
-    // }
-    // for (int j=0; j<8; ++j) {
-    //   iv[i++] = telegram[11];
-    // }
-    // pos = telegram.begin() + 15;
-    // int num_encrypted_bytes = 0;
-    // int num_not_encrypted_at_end = 0;
-
-    // if (decrypt_TPL_AES_CBC_IV(telegram, pos, key, iv,
-    //                           &num_encrypted_bytes, &num_not_encrypted_at_end)) {
     // Seems that AT-WMBUS-08 is not decrypted. Maybe it is not true, but let's try ...
-      size_t i = 11;
-      usage = ((uint32_t)telegram[i+3] << 24) | ((uint32_t)telegram[i+2] << 16) |
-              ((uint32_t)telegram[i+1] << 8)  | ((uint32_t)telegram[i+0]);
-      water_usage = (usage / 3.0) / 1000.0;
-      ret_val = true;
-    // }
+    size_t i = 11;
+    usage = ((uint32_t)telegram[i+3] << 24) | ((uint32_t)telegram[i+2] << 16) |
+            ((uint32_t)telegram[i+1] << 8)  | ((uint32_t)telegram[i+0]);
+    water_usage = (usage / 3.0) / 1000.0;
     return ret_val;
   };
 
--- components/wmbus/version.h
+++ components/wmbus/version.h
@@ -1,3 +1,3 @@
 #ifndef MY_VERSION
-#define MY_VERSION "1.1.2"
+#define MY_VERSION "1.1.3"
 #endif
