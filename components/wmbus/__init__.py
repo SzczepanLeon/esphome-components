387d54bc07d88724f7ba91328d74b7ef2d797aa1
Szczepan <szczepan.staszak@gmail.com>
2023-06-05 20:39:59 +0200

    Add wMBus C1 support - part 9

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -114,7 +114,7 @@ async def to_code(config):
     cg.add_library(
         None,
         None,
-        "https://github.com/SzczepanLeon/wMbus-lib#1.2.8",
+        "https://github.com/SzczepanLeon/wMbus-lib#1.2.9",
     )
 
     cg.add_library(
