42b3580e4039883dffb1d999a41053c821183ca1
Szczepan <szczepan.staszak@gmail.com>
2023-11-13 19:11:36 +0100

    Fix for fix ;-)

--- components/wmbus/wmbus.cpp
+++ components/wmbus/wmbus.cpp
@@ -306,10 +306,6 @@ bool WMBusComponent::decrypt_telegram(std::vector<unsigned char> &telegram, std:
     for (int j=0; j<8; ++j) {
       iv[i++] = telegram[19];
     }
-
-    pos = telegram.begin() + offset;
-    int num_encrypted_bytes = 0;
-    int num_not_encrypted_at_end = 0;
   }
   else {
     ESP_LOGE(TAG, "CI unknown");
@@ -317,6 +313,9 @@ bool WMBusComponent::decrypt_telegram(std::vector<unsigned char> &telegram, std:
   }
 
   if (ret_val) {
+    pos = telegram.begin() + offset;
+    int num_encrypted_bytes = 0;
+    int num_not_encrypted_at_end = 0;
     if (decrypt_TPL_AES_CBC_IV(telegram, pos, key, iv,
                               &num_encrypted_bytes, &num_not_encrypted_at_end)) {
       uint32_t decrypt_check = 0x2F2F;
