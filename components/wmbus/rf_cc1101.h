bbc367cc05c870b92a86ab01d61a5c29e6c2d79a
Szczepan <szczepan.staszak@gmail.com>
2024-01-26 14:02:46 +0100

    Added cc1101 logic

--- components/wmbus/rf_cc1101.h
+++ components/wmbus/rf_cc1101.h
@@ -12,6 +12,53 @@
 #include <string>
 #include <vector>
 
+// CC1101 state machine
+#define MARCSTATE_SLEEP            0x00
+#define MARCSTATE_IDLE             0x01
+#define MARCSTATE_XOFF             0x02
+#define MARCSTATE_VCOON_MC         0x03
+#define MARCSTATE_REGON_MC         0x04
+#define MARCSTATE_MANCAL           0x05
+#define MARCSTATE_VCOON            0x06
+#define MARCSTATE_REGON            0x07
+#define MARCSTATE_STARTCAL         0x08
+#define MARCSTATE_BWBOOST          0x09
+#define MARCSTATE_FS_LOCK          0x0A
+#define MARCSTATE_IFADCON          0x0B
+#define MARCSTATE_ENDCAL           0x0C
+#define MARCSTATE_RX               0x0D
+#define MARCSTATE_RX_END           0x0E
+#define MARCSTATE_RX_RST           0x0F
+#define MARCSTATE_TXRX_SWITCH      0x10
+#define MARCSTATE_RXFIFO_OVERFLOW  0x11
+#define MARCSTATE_FSTXON           0x12
+#define MARCSTATE_TX               0x13
+#define MARCSTATE_TX_END           0x14
+#define MARCSTATE_RXTX_SWITCH      0x15
+#define MARCSTATE_TXFIFO_UNDERFLOW 0x16
+
+#define RX_FIFO_START_THRESHOLD    0
+#define RX_FIFO_THRESHOLD          10  // 44 bytes in Rx FIFO
+
+#define FIXED_PACKET_LENGTH        0x00
+#define INFINITE_PACKET_LENGTH     0x02
+
+
+#define BLOCK1A_SIZE 12     // Size of Block 1, format A
+#define BLOCK1B_SIZE 10     // Size of Block 1, format B
+#define BLOCK2B_SIZE 118    // Maximum size of Block 2, format B
+
+// Helper macros, collides with MSVC's stdlib.h unless NOMINMAX is used
+#ifndef MAX
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#endif
+#ifndef MIN
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
+#include "esphome/core/helpers.h"
+
+
 enum RxLoopState : uint8_t {
   INIT_RX       = 0,
   WAIT_FOR_SYNC = 1,
