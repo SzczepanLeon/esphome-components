981049ca62eb97c85a5b9d53a0dd4929be34791d
Szczepan <szczepan.staszak@gmail.com>
2024-01-26 14:21:34 +0100

    Added cc1101 logic

--- components/wmbus/crc.cpp
+++ components/wmbus/crc.cpp
@@ -3,6 +3,8 @@
 namespace esphome {
 namespace wmbus {
 
+  static const char *TAG = "crc";
+
   static uint16_t crc16(uint8_t const t_message[], uint8_t t_nBytes, uint16_t t_polynomial, uint16_t t_init) {
     uint16_t remainder{t_init};
 
--- components/wmbus/crc.h
+++ components/wmbus/crc.h
@@ -5,7 +5,6 @@
 namespace esphome {
 namespace wmbus {
 
-  static const char *TAG = "crc";
 
   static uint16_t crc16(uint8_t const t_message[], uint8_t t_nBytes, uint16_t t_polynomial, uint16_t t_init);
   static bool crcValid(const uint8_t *t_bytes, uint8_t t_crcOffset);
