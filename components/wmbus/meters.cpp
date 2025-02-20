082bd24f6a457ba67095ab3be82d025fe444fafb
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-20 22:04:44 +0100

    Update meters.cpp

--- components/wmbus/meters.cpp
+++ components/wmbus/meters.cpp
@@ -717,6 +717,7 @@ void MeterCommonImplementation::triggerUpdate(Telegram* t)
     num_updates_++;
     for (auto& cb : on_update_) if (cb) cb(t, this);
     t->handled = true;
+    addStringField("timestamp", "TimeStamp", PrintProperty::HIDE);
     setStringValue("timestamp", datetimeOfUpdateRobot());
 }
 
