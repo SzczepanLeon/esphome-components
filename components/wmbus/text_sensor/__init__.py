6b44b0b6fce2b72b92705a704e176d260667a90e
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-05-21 13:38:47 +0200

    Adjust to ESPHome 2025.5.0

--- components/wmbus/text_sensor/__init__.py
+++ components/wmbus/text_sensor/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.log import Fore, color
+from esphome.log import AnsiFore, color
 from esphome.const import (
     CONF_ID,
     CONF_TYPE,
