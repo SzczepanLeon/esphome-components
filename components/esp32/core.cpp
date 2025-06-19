ea785fd072cc38b1fdd57f30f7f3092d3aad16ac
Szczepan <szczepan.staszak@gmail.com>
2025-06-19 11:05:53 +0200

    Align to ESPHome 2025.6.0

--- components/esp32/core.cpp
+++ components/esp32/core.cpp
@@ -3,11 +3,11 @@
 #include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
 #include "preferences.h"
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
 #include <esp_idf_version.h>
 #include <esp_task_wdt.h>
 #include <esp_timer.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <soc/rtc.h>
 
 #include <hal/cpu_hal.h>
@@ -25,30 +25,38 @@ void loop();
 namespace esphome {
 
 void IRAM_ATTR HOT yield() { vPortYield(); }
-uint32_t IRAM_ATTR HOT millis() { return (uint32_t) (esp_timer_get_time() / 1000ULL); }
+uint32_t IRAM_ATTR HOT millis() {
+  return (uint32_t)(esp_timer_get_time() / 1000ULL);
+}
 void IRAM_ATTR HOT delay(uint32_t ms) { vTaskDelay(ms / portTICK_PERIOD_MS); }
-uint32_t IRAM_ATTR HOT micros() { return (uint32_t) esp_timer_get_time(); }
-void IRAM_ATTR HOT delayMicroseconds(uint32_t us) { delay_microseconds_safe(us); }
+uint32_t IRAM_ATTR HOT micros() { return (uint32_t)esp_timer_get_time(); }
+void IRAM_ATTR HOT delayMicroseconds(uint32_t us) {
+  delay_microseconds_safe(us);
+}
 void arch_restart() {
   esp_restart();
   // restart() doesn't always end execution
-  while (true) {  // NOLINT(clang-diagnostic-unreachable-code)
+  while (true) { // NOLINT(clang-diagnostic-unreachable-code)
     yield();
   }
 }
 
 void arch_init() {
-  // Enable the task watchdog only on the loop task (from which we're currently running)
+  // Enable the task watchdog only on the loop task (from which we're currently
+  // running)
 #if defined(USE_ESP_IDF)
   esp_task_wdt_add(nullptr);
   // Idle task watchdog is disabled on ESP-IDF
 #elif defined(USE_ARDUINO)
   enableLoopWDT();
-  // Disable idle task watchdog on the core we're using (Arduino pins the task to a core)
-#if defined(CONFIG_ESP_TASK_WDT_CHECK_IDLE_TASK_CPU0) && CONFIG_ARDUINO_RUNNING_CORE == 0
+  // Disable idle task watchdog on the core we're using (Arduino pins the task
+  // to a core)
+#if defined(CONFIG_ESP_TASK_WDT_CHECK_IDLE_TASK_CPU0) &&                       \
+    CONFIG_ARDUINO_RUNNING_CORE == 0
   disableCore0WDT();
 #endif
-#if defined(CONFIG_ESP_TASK_WDT_CHECK_IDLE_TASK_CPU1) && CONFIG_ARDUINO_RUNNING_CORE == 1
+#if defined(CONFIG_ESP_TASK_WDT_CHECK_IDLE_TASK_CPU1) &&                       \
+    CONFIG_ARDUINO_RUNNING_CORE == 1
   disableCore1WDT();
 #endif
 #endif
@@ -65,7 +73,8 @@ uint32_t arch_get_cpu_freq_hz() {
   uint32_t freq = 0;
 #ifdef USE_ESP_IDF
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
-  esp_clk_tree_src_get_freq_hz(SOC_MOD_CLK_CPU, ESP_CLK_TREE_SRC_FREQ_PRECISION_CACHED, &freq);
+  esp_clk_tree_src_get_freq_hz(SOC_MOD_CLK_CPU,
+                               ESP_CLK_TREE_SRC_FREQ_PRECISION_CACHED, &freq);
 #else
   rtc_cpu_freq_config_t config;
   rtc_clk_cpu_freq_get_config(&config);
@@ -78,7 +87,8 @@ uint32_t arch_get_cpu_freq_hz() {
 }
 
 #ifdef USE_ESP_IDF
-TaskHandle_t loop_task_handle = nullptr;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+TaskHandle_t loop_task_handle =
+    nullptr; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 void loop_task(void *pv_params) {
   setup();
@@ -91,12 +101,12 @@ extern "C" void app_main() {
   esp32::setup_preferences();
   xTaskCreate(loop_task, "loopTask", 32768, nullptr, 1, &loop_task_handle);
 }
-#endif  // USE_ESP_IDF
+#endif // USE_ESP_IDF
 
 #ifdef USE_ARDUINO
 extern "C" void init() { esp32::setup_preferences(); }
-#endif  // USE_ARDUINO
+#endif // USE_ARDUINO
 
-}  // namespace esphome
+} // namespace esphome
 
-#endif  // USE_ESP32
+#endif // USE_ESP32
