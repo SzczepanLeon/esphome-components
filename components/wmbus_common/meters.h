ba2cbe1859be97da9ad7d224ab8c69cda6aeda41
Johny George <johnygmalayil@gmail.com>
2026-07-19 02:32:40 +0200

    Rename STATUS to WMBUS_STATUS in meters.h

--- components/wmbus_common/meters.h
+++ components/wmbus_common/meters.h
@@ -262,7 +262,7 @@ enum PrintProperty {
                 // or null.
   DEPRECATED = 2, // This field is about to be removed or changed in a newer
                   // driver, which will have a new name.
-  STATUS = 4, // This is >the< status field and it should read OK of not error
+  WMBUS_STATUS = 4, // This is >the< status field and it should read OK of not error
               // flags are set.
   INCLUDE_TPL_STATUS = 8,  // This text field also includes the tpl status
                            // decoding. multiple OK:s collapse to a single OK.
